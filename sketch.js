6801daa4148cc10eb9a0fa4f514677322fc97ff7
Hanbee17 <hanbee.cho@sjsu.edu>
2022-03-17 01:32:30 -0700

    added more elements

--- sketch.js
+++ sketch.js
@@ -1,79 +1,123 @@
-
 let title = ['Prelude',
-             'One Night, Counting the Stars',
-             'A Poem Easily Written'];
+  'One Night, Counting the Stars',
+  'A Poem Easily Written',
+  'Confessions',
+  'The Cross'
+];
 
 let ktitle = ['서시',
-              '별 헤는 밤',
-              '쉽게 쓰여진 시'];
+  '별 헤는 밤',
+  '쉽게 쓰여진 시',
+  '참회록',
+  '십자가'
+];
 
-let poem1 = ['Till my dying day, let no spot of shame be upon me, as I look to heaven;',
-             'I was troubled even by a slight wind that grew on a leaf.',
-             'I will love all that is dying with a heart that sings of stars',
-             'And walk the pate that is given me.',
-             'Even tonight a star is rustled by the wind.'];
+let poem1 = ['Till my dying day, let no spot of shame be upon me, as I look to heaven',
+  'I was troubled even by a slight wind that grew on a leaf.',
+  'I will love all that is dying with a heart that sings of stars',
+  'And walk the pate that is given me.',
+  'Even tonight a star is rustled by the wind.'
+];
 
 let kpoem1 = ['죽는날 까지 하늘을 우러러 한점 부끄럼이 없기를',
-              '잎새에 이는 바람에도 나는 괴로워 했다.',
-              '별을 노래하는 마음으로 모든 죽어가는 것을 사랑해야지',
-              '그리고 나에게 주어진 길을 걸어가야겠다.',
-              '오늘밤에도 별이 바람에 스치운다.']
+  '잎새에 이는 바람에도 나는 괴로워 했다.',
+  '별을 노래하는 마음으로 모든 죽어가는 것을 사랑해야지',
+  '그리고 나에게 주어진 길을 걸어가야겠다.',
+  '오늘밤에도 별이 바람에 스치운다.'
+]
 
 let poem2 = ['The sky when the season passes is filled to the full with the fall.',
-             'I could almost count, free of care, all the stars in the folds of this fall.',
-             'If I cannot complete the counting of stars that are etched one and two in my heart,',
-             'It is that morn arrives too hastily, that tomorrow night is yet to come, and that my youth is not yet done.',
-             'A star and rememberance, A star and love, A star and lonethisPoemss',
-             'A star and longing, A tar and poem, A star and mother, mother.',
-             'Mother, for each star I sing the syllables of beautiful words.',
-             'Names of children who sat next to me in school, names of foreign girls like ‘Pae,’ ‘Kyeong,’ ‘Ok’; names of girls already become mothers;',
-             'names of poor neighbors; pigeon, puppy, rabbit, donkey, deer; names of poets like ‘Francis Jammes,’ ‘Reiner Maria Rilke.’',
-             'These are too distant. Distant as the shimmering stars.',
-             'Mother, and, you are distant in Bukkando.',
-             'For whatever longing, On a hill showered with the rays of numerous stars,',
-             'I spelled out my name on the earth and covered it over with soil.',
-             'Perhaps, even, the cricket cries all night sadly ashamed of its own name.',
-             'But when winter passes and spring comes to my star, just as green grass grows on burial mounds,',
-             'so will it grow rich and proud on the hill where my name is buried.'];
+  'I could almost count, free of care, all the stars in the folds of this fall.',
+  'If I cannot complete the counting of stars that are etched one and two in my heart,',
+  'It is that morn arrives too hastily, that tomorrow night is yet to come, and that my youth is not yet done.',
+  'A star and rememberance, A star and love, A star and lonethisPoemss',
+  'A star and longing, A tar and poem, A star and mother, mother.',
+  'Mother, for each star I sing the syllables of beautiful words.',
+  'Names of children who sat next to me in school, names of foreign girls like ‘Pae,’ ‘Kyeong,’ ‘Ok’; names of girls already become mothers;',
+  'names of poor neighbors; pigeon, puppy, rabbit, donkey, deer; names of poets like ‘Francis Jammes,’ ‘Reiner Maria Rilke.’',
+  'These are too distant. Distant as the shimmering stars.',
+  'Mother, and, you are distant in Bukkando.',
+  'For whatever longing, On a hill showered with the rays of numerous stars,',
+  'I spelled out my name on the earth and covered it over with soil.',
+  'Perhaps, even, the cricket cries all night sadly ashamed of its own name.',
+  'But when winter passes and spring comes to my star, just as green grass grows on burial mounds,',
+  'so will it grow rich and proud on the hill where my name is buried.'
+];
 
 let kpoem2 = ['계절이 지나가는 하늘에는 가을로 가득 차 있습니다.',
-             '나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다.',
-             '가슴속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은',
-             '쉬이 아침이 오는 까닭이요, 내일 밤이 남은 까닭이요, 아직 나의 청춘이 다하지 않은 까닭입니다.',
-             '별 하나에 추억과, 별 하나에 사랑과, 별 하나에 쓸쓸함과',
-             '별 하나에 동경과, 별 하나에 시와, 별 하나에 어머니, 어머니,',
-             '어머님, 나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다.',
-             '소학교 때 책상을 같이 했던 아이들의 이름과, 패, 경, 옥, 이런 이국 소녀들의 이름과,벌써 아기 어머니 된 계집애들의 이름과,',
-             '가난한 이웃 사람들의 이름과, 비둘기, 강아지, 토끼, 노새, 노루, ‘프랑시스 잠’, ‘라이너 마리아 릴케’, 이런 시인의 이름을 불러 봅니다.',
-             '이네들은 너무나 멀리 있습니다. 별이 아슬히 멀듯이.',
-             '어머님, 그리고 당신은 멀리 북간도에 계십니다.',
-             '나는 무엇인지 그리워 이 많은 별빛이 내린 언덕 위에',
-             '내 이름자를 써 보고, 흙으로 덮어 버리었습니다.',
-             '딴은, 밤을 새워 우는 벌레는 부끄러운 이름을 슬퍼하는 까닭입니다.',
-             '그러나 겨울이 지나고 나의 별에도 봄이 오면 무덤 위에 파란 잔디가 피어나듯이',
-             '내 이름자 묻힌 언덕 위에도 자랑처럼 풀이 무성할 게외다.']
+  '나는 아무 걱정도 없이 가을 속의 별들을 다 헤일 듯합니다.',
+  '가슴속에 하나 둘 새겨지는 별을 이제 다 못 헤는 것은',
+  '쉬이 아침이 오는 까닭이요, 내일 밤이 남은 까닭이요, 아직 나의 청춘이 다하지 않은 까닭입니다.',
+  '별 하나에 추억과, 별 하나에 사랑과, 별 하나에 쓸쓸함과',
+  '별 하나에 동경과, 별 하나에 시와, 별 하나에 어머니, 어머니,',
+  '어머님, 나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다.',
+  '소학교 때 책상을 같이 했던 아이들의 이름과, 패, 경, 옥, 이런 이국 소녀들의 이름과,벌써 아기 어머니 된 계집애들의 이름과,',
+  '가난한 이웃 사람들의 이름과, 비둘기, 강아지, 토끼, 노새, 노루, ‘프랑시스 잠’, ‘라이너 마리아 릴케’, 이런 시인의 이름을 불러 봅니다.',
+  '이네들은 너무나 멀리 있습니다. 별이 아슬히 멀듯이.',
+  '어머님, 그리고 당신은 멀리 북간도에 계십니다.',
+  '나는 무엇인지 그리워 이 많은 별빛이 내린 언덕 위에',
+  '내 이름자를 써 보고, 흙으로 덮어 버리었습니다.',
+  '딴은, 밤을 새워 우는 벌레는 부끄러운 이름을 슬퍼하는 까닭입니다.',
+  '그러나 겨울이 지나고 나의 별에도 봄이 오면 무덤 위에 파란 잔디가 피어나듯이',
+  '내 이름자 묻힌 언덕 위에도 자랑처럼 풀이 무성할 게외다.'
+]
 
 let poem3 = ['Night rain outside the window, lightly whispers this spacious room is no country of mine.',
-            'A poet’s is a sad calling, I know, yet shall I attempt a thisPoem of verse.',
-            'Tuition in envelope, sent to me with the warm smell of sweat and love,',
-            'and college notebook tucked under arm, I attend an old professor lecturing.',
-            'I ask myself, having lost, one, two, all of my friends of childhood,',
-            'for what expectation do I so, alone, sink into thought?',
-            'For a poem to be so easily written, when life is so difficult,is a matter of shame.',
-            'This spacious room is no country of mine, lightly whispers the night rain outside the window.',
-            'The last I, lighting a lamp to push back the dark, and waiting for morning that comes like an age,',
-            'I hold a small hand out to myself, the first ever hand-holding of tears and consolation.'];
+  'A poet’s is a sad calling, I know, yet shall I attempt a thisPoem of verse.',
+  'Tuition in envelope, sent to me with the warm smell of sweat and love,',
+  'and college notebook tucked under arm, I attend an old professor lecturing.',
+  'I ask myself, having lost, one, two, all of my friends of childhood,',
+  'for what expectation do I so, alone, sink into thought?',
+  'For a poem to be so easily written, when life is so difficult,is a matter of shame.',
+  'This spacious room is no country of mine, lightly whispers the night rain outside the window.',
+  'The last I, lighting a lamp to push back the dark, and waiting for morning that comes like an age,',
+  'I hold a small hand out to myself, the first ever hand-holding of tears and consolation.'
+];
 
 let kpoem3 = ['창밖에 밤비가 속살거려 육척방은 남의 나라.',
-              '시인이란 슬픈 천명인 줄 알면서도 한 줄 시를 적어 볼까.',
-              '땀내와 사랑내 포근히 품긴 보내 주신 학비 봉투를 받아,',
-              '대학 노트를 끼고 늙은 교수의 강의를 들으러 간다.',
-              '생각해 보면 어릴 때 동무들 하나, 둘, 죄다 잃어버리고,',
-              '나는 무얼 바라 나는 다만, 홀로 침전하는 것일까?',
-              '인생은 살기 어렵다는데 시가 이렇게 쉽게 씌어지는 것은 부끄러운 일이다.',
-              '육척방은 남의 나라 창밖에 밤비가 속살거리는데,',
-              '등불처럼 어둠을 조금 내몰고 시대처럼 올 아침을 기다리는 최후의 나,',
-              '나는 나에게 작은 손을 내밀어 눈물과 위안으로 잡는 최초의 악수.']
+  '시인이란 슬픈 천명인 줄 알면서도 한 줄 시를 적어 볼까.',
+  '땀내와 사랑내 포근히 품긴 보내 주신 학비 봉투를 받아,',
+  '대학 노트를 끼고 늙은 교수의 강의를 들으러 간다.',
+  '생각해 보면 어릴 때 동무들 하나, 둘, 죄다 잃어버리고,',
+  '나는 무얼 바라 나는 다만, 홀로 침전하는 것일까?',
+  '인생은 살기 어렵다는데 시가 이렇게 쉽게 씌어지는 것은 부끄러운 일이다.',
+  '육척방은 남의 나라 창밖에 밤비가 속살거리는데,',
+  '등불처럼 어둠을 조금 내몰고 시대처럼 올 아침을 기다리는 최후의 나,',
+  '나는 나에게 작은 손을 내밀어 눈물과 위안으로 잡는 최초의 악수.'
+]
+
+let poem4 = ['In the copper mirror rusted blue, My face still remains. Now, which kingdom’s relic is it, That brings shame to the kingdom?',
+  'So, I condense my confessions into a single line.',
+  ' -- What joys have I expected that I’ve lived, These twenty four years and a month?',
+  'Tomorrow, the day after tomorrow, Or on a happy day to come, I must write one more line.',
+  '-- At that time when I was so young, Why did I write such a shameful confession?',
+  'Now, let me polish my mirror, With my palms and soles night after night.',
+  'Then a turned back of a sad man, Walking all alone, Under a meteorite appears in the mirror.'
+]
+
+let kpoem4 = ['파란 녹이 낀 구리 거울 속에 내 얼굴이 남아 있는 것은, 어느 왕조의 유물이기에 이다지도 욕될까.',
+  '나는 나의 참회의 글을 한 줄에 줄이자.',
+  '--만 이십사 년 일 개월을 무슨 기쁨을 바라 살아왔던가.',
+  '내일이나 모레나 그 어느 즐거운 날에 나는 또 한 줄의 참회록을 써야 한다.',
+  '--그때 그 젊은 나이에 왜 그런 부끄런 고백을 했던가.',
+  '밤이면 밤마다 나의 거울을 손바닥으로 발바닥으로 닦아 보자.',
+  '그러면 어느 운석 밑으로 홀로 걸어가는 슬픈 사람의 뒷모양이 거울 속에 나타나 온다.'
+]
+
+let poem5 = ['It was a sunlight chasing me, but now it hangs on a cross on the top of a chapel.',
+  'How does it climb atop, the spire being so high ? ',
+  'No church bells are ringing, but I’ll hang around whistling, and',
+  'If, as if to the man in agony, happy Jesus Christ, the cross is permitted me',
+  'Neck long stretched out, under the darkening sky, I will quietly shed blood blooming like a flower.'
+]
+
+let kpoem5 = ['쫓아오던 햇빛인데, 지금 교회당 꼭대기 십자가에 걸리었습니다.',
+  '첨탑이 저렇게도 높은데, 어떻게 올라갈 수 있을까요.',
+  '종 소리도 들려오지 않는데 휘파람이나 불며 서성거리다가,',
+  '괴로웠던 사나이, 행복한 예수 그리스도에게 처럼 십자가가 허락된다면',
+  '모가지를 드리우고 꽃처럼 피어나는 피를 어두워 가는 하늘 밑에 조용히 흘리겠습니다.'
+]
+
 
 let textEx = 'Yoon Dongju (1917-1945), the poet most loved by the Korean people, uprightly sought the direction of the times and life through literature, even in difficult times of the colonial rule. He consistently wrote poems in Korean that pondered and reflected on the reality the community faced, and this led to his imprisonment on charges of independence movement and death at the age of 27. However, the poems that put him in danger have since resonated and breathed in the hearts of many.'
 let textEx2 = 'The 124 poems and prose works that he left behind have become the poems most loved by Koreans, remembered by East Asians, and sympathized with by people around the world. Through poems such as "Prelude", "The Cross", and "Counting the Stars", he showed that a clear  and pure soul lives on earth. He pledged to ‘walk the path given to him’ with a heart of ‘love for all dying things.’ The poems of Yoon Dongju, who constantly reflected on himself and sought to walk by his faith, have since become the driving force in each era and for the youth.'
@@ -85,12 +129,16 @@ let thisPoem;
 let RanPoem1;
 let RanPoem2;
 let RanPoem3;
+let RanPoem4;
+let RanPoem5;
 
 let myFont;
 let myImg0;
 let myImg1;
 let myImg2;
 let myImg3;
+let myImg4;
+let myImg5;
 let myBgm;
 
 function preload() {
@@ -99,6 +147,8 @@ function preload() {
   myImg1 = loadImage('img1.jpg');
   myImg2 = loadImage('img2.jpg');
   myImg3 = loadImage('img3.jpg');
+  myImg4 = loadImage('img4.jpg');
+  myImg5 = loadImage('img5.jpg');
   myBgm = loadSound('bgm.mp3');
 }
 
@@ -116,10 +166,10 @@ function setup() {
 
 function draw() {
   push();
-  textSize (20);
-  fill(204,204,204);
+  textSize(20);
+  fill(204, 204, 204);
   textAlign(CENTER);
-  text('Click anywhere to see the poetry.', width*0.5, height*0.065);
+  text('Click anywhere to see the poetry.', width * 0.5, height * 0.065);
   pop();
 }
 
@@ -139,60 +189,79 @@ function mousePressed() {
 }
 
 function intText() {
-  fill(204,204,204);
+  fill(204, 204, 204);
   imageMode(CENTER);
-  image(myImg0,width*0.3,height*0.5, width*0.4, height*0.7);
+  image(myImg0, width * 0.3, height * 0.5, width * 0.4, height * 0.7);
   textSize(20)
   textAlign(CENTER);
-  text('Translated by Suh Hong Won and Seon-Gyeom Choi, 2011 by YONSEI UNIVERSITY PRESS.', width*0.5, height*0.90);
-  text('Images from Yoon Dongju Memorial Hall, Yonsei University', width*0.5, height*0.935);
-  text('Music from Keys of Moon music, https://soundcloud.com/keysofmoon', width*0.5, height*0.97);
+  text('Translated by Suh Hong Won and Seon-Gyeom Choi, 2011 by YONSEI UNIVERSITY PRESS.', width * 0.5, height * 0.90);
+  text('Images from Yoon Dongju Memorial Hall, Yonsei University', width * 0.5, height * 0.935);
+  text('Music from Keys of Moon music, https://soundcloud.com/keysofmoon', width * 0.5, height * 0.97);
 
   push();
   fill(255);
   rectMode(CENTER);
   textAlign(LEFT);
   textSize(25);
-  text(textEx, width*0.75,height*0.6, width*0.4, height*0.9)
-  text(textEx2, width*0.75,height*0.95, width*0.4, height*0.9)
+  text(textEx, width * 0.75, height * 0.6, width * 0.4, height * 0.9)
+  text(textEx2, width * 0.75, height * 0.95, width * 0.4, height * 0.9)
   pop();
 }
 
 function fortText() {
-  background(254,250,224);
+  background(254, 250, 224);
   //intText();
 
   push();
-  textSize (25);
-  fill(33,37,41);
+  textSize(25);
+  fill(33, 37, 41);
   textAlign(CENTER);
 
   thisTitle = int(random(title.length));
   if (thisTitle == 0) {
-    image(myImg1, width*0.5,height*0.4, width*0.6, height*0.6);
+    image(myImg1, width * 0.5, height * 0.4, width * 0.6, height * 0.6);
     thiskTitle = ktitle[0]
     RanPoem1 = int(random(poem1.length));
     thisPoem = poem1[RanPoem1];
     thisKpoem = kpoem1[RanPoem1];
 
   } else if (thisTitle == 1) {
-    image(myImg2, width*0.5,height*0.4, width*0.6, height*0.6);
+    image(myImg2, width * 0.5, height * 0.4, width * 0.6, height * 0.6);
     thiskTitle = ktitle[1]
     RanPoem2 = int(random(poem2.length));
     thisPoem = poem2[RanPoem2];
     thisKpoem = kpoem2[RanPoem2];
 
   } else if (thisTitle == 2) {
-    image(myImg3, width*0.5,height*0.4, width*0.6, height*0.6);
+    image(myImg3, width * 0.5, height * 0.4, width * 0.6, height * 0.6);
     thiskTitle = ktitle[2]
     RanPoem3 = int(random(poem3.length));
     thisPoem = poem3[RanPoem3];
     thisKpoem = kpoem3[RanPoem3];
+
+  } else if (thisTitle == 3) {
+    image(myImg4, width * 0.5, height * 0.4, width * 0.6, height * 0.6);
+    thiskTitle = ktitle[3]
+    RanPoem4 = int(random(poem4.length));
+    thisPoem = poem4[RanPoem4];
+    thisKpoem = kpoem4[RanPoem4];
+
+  } else if (thisTitle == 4) {
+    image(myImg5, width * 0.5, height * 0.4, width * 0.6, height * 0.6);
+    thiskTitle = ktitle[4]
+    RanPoem5 = int(random(poem5.length));
+    thisPoem = poem5[RanPoem5];
+    thisKpoem = kpoem5[RanPoem5];
   }
 
-  text(title[thisTitle], width*0.5, height*0.75);
-  text(thiskTitle, width*0.5, height*0.8);
-  text(thisPoem, width*0.5, height*0.9);
-  text(thisKpoem, width*0.5, height*0.95)
+  text(title[thisTitle], width * 0.5, height * 0.75);
+  text(thiskTitle, width * 0.5, height * 0.8);
+
+  push();
+  textSize(28);
+  text(thisPoem, width * 0.5, height * 0.875);
+  text(thisKpoem, width * 0.5, height * 0.925)
+
+  pop();
   pop();
 }
